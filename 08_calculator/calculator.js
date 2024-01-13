c60ffee610f636548eedd08f14f029340a6212c5
Sunil-Hegde <sunil1hegde@gmail.com>
2024-01-13 23:12:52 +0530

    completed 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	var sum = 0;
+  for(let i =0; i<array.length; i++){
+    sum+=array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  var product = 1;
+  for(let i =0; i<array.length; i++){
+    product*=array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+	var powerProd = 1;
+  if (b === 0){
+    return powerProd;
+  } else {
+    for(var i = 0; i < b; i++){
+      powerProd*= a;
+    }
+    return powerProd;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  var fact = 1;
+  if (number === 0 || number === 1){
+    return fact;
+  } else {
+    fact = number * factorial(number-1);
+    return fact;
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let x = newString.length-1;
+    let i = 0;
+    while(i<x){
+        if (newString.charAt(i) === newString.charAt(x-i)){
+            i++;
+        } else {
+            return false;
+        }
+        i++;
+    }
+    return true;
 };
 
 // Do not edit below this line
